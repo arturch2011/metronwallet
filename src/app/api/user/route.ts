87bbff8211282871cc2be7d57e614222c215f982
Lucasch1 <lucasch2011@gmail.com>
2024-11-09 23:10:00 +0700

    adição da rota para pegar a carteira do usuario

---
+++ src/app/api/user/route.ts
@@ -0,0 +1,62 @@
+import { NextResponse, NextRequest } from "next/server";
+import { createSmartAccountClient } from "@biconomy/account";
+import {
+    collection,
+    doc,
+    getDocs,
+    query,
+    setDoc,
+    getDoc,
+} from "firebase/firestore";
+import { createUserWithEmailAndPassword } from "firebase/auth";
+import { ethers } from "ethers";
+import { db, auth } from "@/firebase/config";
+
+export const POST = async (req: NextRequest) => {
+    try {
+        const url = process.env.RPC_URL as string;
+        const data = await req.json();
+        const { id } = data;
+
+        const userBase = doc(db, "users", id);
+        const userDoc = await getDoc(userBase);
+
+        if (!userDoc.exists()) {
+            return new NextResponse(
+                JSON.stringify({ message: "user dont exists" }),
+                { status: 400 }
+            );
+        }
+        const userData = userDoc.data();
+        const provider = new ethers.JsonRpcProvider(url);
+        const signer = new ethers.Wallet(
+            `${process.env.WALLET_PRIVATE_KEY}`,
+            provider
+        );
+
+        const biconomySmartAccountConfig = {
+            signer: signer,
+            bundlerUrl: process.env.BUNDLER_URL as string,
+            biconomyPaymasterApiKey: process.env.PAYMASTER_API_KEY,
+        };
+
+        const smartAccount = await createSmartAccountClient(
+            biconomySmartAccountConfig
+        );
+        const smartWallet = await smartAccount.getAccountAddress({
+            index: userData.idWallet,
+        });
+
+        return new NextResponse(
+            JSON.stringify({
+                message: "user data found",
+                userWallet: smartWallet,
+            }),
+            { status: 200 }
+        );
+    } catch (error) {
+        return new NextResponse(JSON.stringify({ message: "internal error" }), {
+            status: 500,
+        });
+    }
+};

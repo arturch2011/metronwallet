39bbd7c238f728ffe0eee9be0e8ef68ce8902407
dzns <davidkim1131@gmail.com>
2024-11-10 10:32:29 +0700

    ajusted with main

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,8 +3,11 @@
 import { Skeleton } from "@/components/ui/Skeleton";
 import { TokenList } from "@/components/wallet/TokenList";
 import { WalletCard } from "@/components/wallet/WalletCard";
+import { useAuth } from "@/providers/user-context";
 
 export default function Home() {
+  const { user } = useAuth();
+  console.log(user)
   return (
     <Skeleton>
       <WalletCard />
